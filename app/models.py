71820a76ce546fcf758dccb9a074040c7fc615aa
Hunter <hvanlear@gmail.com>
2020-11-17 13:13:07 -0500

    added Post, PostTag and Tag models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 
 class User(db.Model):
@@ -8,6 +9,35 @@ class User(db.Model):
     username = db.Column(db.Text, nullable=False, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    heading = db.Column(db.String(100), nullable=False)
+    post = db.Column(db.String(15000), nullable=False)
+    update_date = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
+    create_date = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    tags = db.relationship(
+        'Tag', secondary='post_tag', backref='posts')
+
+
+class PostTag(db.Model):
+    __tablename__ = 'post_tag'
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(15), nullable=False)
