9b4c50ab74355545ea1cdea7090970660a2bf956
Hunter <hvanlear@gmail.com>
2021-03-09 12:37:44 -0500

    added resume pdf to about page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for, redirect, flash, request
-from app import  db
+from flask import render_template, url_for, redirect, flash, request, make_response, send_file, url_for, send_from_directory
+from app import db
 from app.main import bp
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post, Project, Tag
@@ -13,14 +13,21 @@ def index():
     projects = Project.query.all()
     return render_template('main/index.html', title='Home', posts=posts, projects=projects)
 
+
 @bp.route('/index/contact')
 def contact():
-    #must be a better way of doing this
+    # must be a better way of doing this
     posts = Post.query.order_by(Post.create_date.desc()).limit(2).all()
     projects = Project.query.all()
-    return render_template('main/index.html',posts=posts, projects=projects, scrollToAnchor="contact_section" )
+    return render_template('main/index.html', posts=posts, projects=projects, scrollToAnchor="contact_section")
 
 
 @bp.route('/about')
 def show_about():
     return render_template('main/about.html', title='Me')
+
+
+@bp.route('/resume')
+def show_resume():
+
+    return send_file('static/assets/resume.pdf', attachment_filename='resume.pdf')
